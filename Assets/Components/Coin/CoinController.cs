d270f429c2881a3d07af6228c0c238ae89d0dc16
JJCanon <jjcanond@eafit.edu.co>
2024-05-14 23:54:54 -0500

    implementacion de sonido en monedas y cajas

--- Assets/Components/Coin/CoinController.cs
+++ Assets/Components/Coin/CoinController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider c){
-        if(c.gameObject.tag == "Player"){
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
             ScoreController.coinCount += 1;
             Destroy(gameObject);
         }
