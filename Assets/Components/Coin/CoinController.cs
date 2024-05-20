6a09f636eac86813f561386a53dbfd56ce2ed4b5
Juan Manuel Valencia Zapata <jmvalenciz@protonmail.com>
2024-05-20 14:14:01 -0500

    Add audio to coins and crates

--- Assets/Components/Coin/CoinController.cs
+++ Assets/Components/Coin/CoinController.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinController : MonoBehaviour
-{
+public class CoinController : MonoBehaviour {
+    public AudioSource audioSource;
+
     private void OnTriggerEnter(Collider c){
         if(c.gameObject.tag == "Player"){
             ScoreController.coinCount += 1;
-            Destroy(gameObject);
+            this.audioSource.Play();
+            GetComponent<Collider>().enabled = false;
+            GetComponent<MeshRenderer>().enabled = false;
+            StartCoroutine(DestroyAfterAudio());
         }
+    }
+    private IEnumerator DestroyAfterAudio() {
+        yield return new WaitForSeconds(audioSource.clip.length);
+        Destroy(gameObject);
     }
 }
